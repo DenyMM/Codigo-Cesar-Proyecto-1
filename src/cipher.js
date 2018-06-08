4705edc94031ac00aa452b2e9f780adc92d2562e
DenyMM <deny.lolipop@gmail.com>
2018-06-08 08:36:47 -0500

    Ya codifica

---
+++ src/cipher.js
@@ -0,0 +1,29 @@
+const cipherTextOp = (string , offset)=>{
+  let result = ""; // no se puede operar con string vacio
+  let ciphText = result;    //asignarle un valor nuevo como variable
+  for (let i = 0; i < string.length; i++){   //empieza desde 0 hasta el numero final de la palabra variable string, se incremente 1
+    //Mayusculas, en la tabla ASCII aparecen del 65-90
+    if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+      ciphText = (((string.charCodeAt(i)-65) + offset) % 26)+65;
+    }
+    //Minusculas, en la tabla ASCII aparecen del 97 al 122
+    else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+      ciphText = (((string.charCodeAt(i)-97) + offset) % 26) + 97;
+    }
+    //Strings unicos, en la tabla ASCII aparecen del 32 al 64
+    else if (string.charCodeAt(i)>=32 && string.charCodeAt(i)<=64){
+      ciphText = (((string.charCodeAt(i) - 32)+offset) % 26) + 32;
+    }
+    result= result + String.fromCharCode(ciphText); /// //reescribe cifrado convierte el ascii a la letra String.fromCharCode()
+  }
+  return result;
+}
+function final(){
+  let textIntUser = document.getElementById('intUserText').value; //agarra el texto del usuario
+  // Para introducir en una variable el numero de posiciones que de el usuario y usarlo en vex de 33
+//  let poscIntUser = document.getElementById('numPosicUser').value;
+document.getElementById('ciphTextHere').innerHTML=(cipherTextOp(textIntUser,33)); //lanza la operacion (funcion(string,posicion))
+}
+//window.cipher = {
+  // ...
+//};
---
+++ src/index.js
@@ -0,0 +1,14 @@
+function showword(){
+  var getTextUser =document.getElementById('intUserText').value;
+  document.getElementById('textToCiph').innerHTML=("El palabra que quieres codificar es:"+ getTextUser);
+ }
+
+ // Funcion para la posicion que introduce el usuario
+//function showposc(){
+ //var userNum = document.getElementById('numPosicUser').value;
+//document.getElementById('numPoscHere').innerHTML= ("El numero que quieres decodificar es:"+ userNum);
+//  }
+
+
+//function goToCiph() {
+//window.location.href = "cipher.js";}
