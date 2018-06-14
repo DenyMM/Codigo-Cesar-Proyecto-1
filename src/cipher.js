e1ef4d2123a9a6c8603d6c6f7de9b7ce9e902575
DenyMM <deny.lolipop@gmail.com>
2018-06-14 12:39:09 -0500

    Se agrego el window cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,42 +1,47 @@
-window.cipher = (string , offset)=>{
-  let result = ""; // no se puede operar con string vacio
-  let ciphText = result;    //asignarle un valor nuevo como variable
-  for (let i = 0; i < string.length; i++){   //empieza desde 0 hasta el numero final de la palabra variable string, se incremente 1
-    if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-     ciphText = (((string.charCodeAt(i)-65) + offset) % 26)+65;
-     }
-     else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
-     ciphText = (((string.charCodeAt(i)-97) + offset) % 26) + 97;
-     }
-     else if (string.charCodeAt(i)>=32 && string.charCodeAt(i)<=64){
-     ciphText = (((string.charCodeAt(i) - 32)+offset) % 26) + 32;
-     }
-     result= result + String.fromCharCode(ciphText); /// //reescribe cifrado convierte el ascii a la letra String.fromCharCode()
-     }
-     return result;
-     }
- const finalCyph=()=>{
-  let textIntUser = document.getElementById('intUserText').value;
-  document.getElementById('ciphTextHere').innerHTML=(window.cipher(textIntUser,33)); //lanza la operacion (funcion(string,posicion))
+
+  window.cipher ={
+   offset:33,
+  encode:(string, offset)=>{
+    //const FunCipher = (string , offset)=>{
+      let result = ""; // no se puede operar con string vacio
+      let ciphText = result;    //asignarle un valor nuevo como variable
+      for (let i = 0; i < string.length; i++){   //empieza desde 0 hasta el numero final de la palabra variable string, se incremente 1
+        if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+         ciphText = (((string.charCodeAt(i)-65) + offset) % 26)+65;
+         }
+         else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+         ciphText = (((string.charCodeAt(i)-97) + offset) % 26) + 97;
+         }
+         else if (string.charCodeAt(i)>=32 && string.charCodeAt(i)<=64){
+         ciphText = (((string.charCodeAt(i) - 32)+offset) % 26) + 32;
+         }
+         result= result + String.fromCharCode(ciphText); /// //reescribe cifrado convierte el ascii a la letra String.fromCharCode()
+         }
+         return result;
+      //   }
+
+  },
+  decode:(string, offset)=>{
+    //  const FunDecipher =(string , offset)=>{
+        let resultD = "";
+        let cyphNum = resultD;
+        for (let i = 0; i < string.length; i++){
+          if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+            cyphNum = (((string.charCodeAt(i)+65) - offset) % 26) +65;
+            }
+            else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+            cyphNum = (((string.charCodeAt(i)+97) + offset) % 26) + 97;
+            }
+            else if (string.charCodeAt(i)>=32 && string.charCodeAt(i)<=64){
+            cyphNum = (((string.charCodeAt(i) - 32) + offset) % 26) + 32;
+            }
+              resultD = resultD + String.fromCharCode(cyphNum);
+            }
+            return resultD;
+      //      }
+
+  },
+  createCipherWithOffset:()=>{
+
   }
-window.decipher =(string , offset)=>{
-  let resultD = "";
-  let cyphNum = resultD;
-  for (let i = 0; i < string.length; i++){
-    if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-      cyphNum = (((string.charCodeAt(i)+65) - offset) % 26) +65;
-      }
-      else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
-      cyphNum = (((string.charCodeAt(i)+97) + offset) % 26) + 97;
-      }
-      else if (string.charCodeAt(i)>=32 && string.charCodeAt(i)<=64){
-      cyphNum = (((string.charCodeAt(i) - 32) + offset) % 26) + 32;
-      }
-        resultD = resultD + String.fromCharCode(cyphNum);
-      }
-      return resultD;
-      }
-  const finalDeCyph=()=>{
-  let numIntUser = document.getElementById('intUserNum').value;
-  document.getElementById('ciphNumHere').innerHTML= (window.decipher(numIntUser,33));
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 const showword=()=>{
   var getTextUser =document.getElementById('intUserText').value;
   document.getElementById('textToCiph').innerHTML=("El palabra que quieres codificar es:"+ getTextUser);
@@ -9,6 +10,13 @@ const shownum=()=>{
   document.getElementById('numToCyph').innerHTML= ("El numero que quieres decodificar es:"+ getNumUser);
   }
 
-
+  const finalCyph=()=>{
+   let textIntUser = document.getElementById('intUserText').value;
+   document.getElementById('ciphTextHere').innerHTML=(cipher.encode(textIntUser,33)); //lanza la operacion (funcion(string,posicion))
+   }
+   const finalDeCyph=()=>{
+   let numIntUser = document.getElementById('intUserNum').value;
+   document.getElementById('ciphNumHere').innerHTML= (cipher.decode(numIntUser,33));
+ }
 //function goToCiph() {
 //window.location.href = "cipher.js";}
